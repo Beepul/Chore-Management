9eb5752a875423e4ec429b6f625727ee9ee313fc
Beepul <beepulmagar@gmail.com>
2026-05-30 21:46:14 +1000

    github action runner trigger

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ async function start() {
   const PORT = process.env.PORT || 5001;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
+    console.log("==============================")
   });
 }
 
